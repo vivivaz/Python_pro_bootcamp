0fba0f2e3249d510d0a14fe41c4b8c48ac8a2819
vivivaz <vivianleocardovaz@gmail.com>
2023-05-20 23:06:15 -0300

    day 8.1

---
+++ day8/day8_1.py
@@ -0,0 +1,17 @@
+# Review: 
+# Create a function called greet(). 
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+
+def greet():
+    print("hello")
+    print("test")
+    print("function")
+
+greet()
+
+def greet_with(name, location):
+    print(f"hello {name}")
+    print(f"what is it like in {location}?")
+
+greet_with(location = "Rio", name = "Vivian")
