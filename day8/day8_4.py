2629dc48f21d45b5aa08eee4e6b75ee2607f9139
vivivaz <vivianleocardovaz@gmail.com>
2023-05-21 21:45:42 -0300

    day 8.4

---
+++ day8/day8_4.py
@@ -0,0 +1,65 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+#TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
+
+    #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
+    #e.g. 
+    #plain_text = "hello"
+    #shift = 5
+    #cipher_text = "mjqqt"
+    #print output: "The encoded text is mjqqt"
+
+    ##HINT: How do you get the index of an item in a list:
+    #https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
+
+    ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
+
+#TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
+
+def encrypt(text, shift):
+    text = list(text)
+    message = []
+    for i in text:
+        n = 0
+        for j in alphabet:
+            if j == i:
+                break
+            n += 1
+        message += alphabet[(n + shift) % len(alphabet)]
+    message = ''.join(message)
+    print(f"The encoded text is {message}")
+
+#TODO-1: Create a different function called 'decrypt' that takes the 'text' and 'shift' as inputs.
+
+  #TODO-2: Inside the 'decrypt' function, shift each letter of the 'text' *backwards* in the alphabet by the shift amount and print the decrypted text.  
+  #e.g. 
+  #cipher_text = "mjqqt"
+  #shift = 5
+  #plain_text = "hello"
+  #print output: "The decoded text is hello"
+
+
+#TODO-3: Check if the user wanted to encrypt or decrypt the message by checking the 'direction' variable. Then call the correct function based on that 'drection' variable. You should be able to test the code to encrypt *AND* decrypt a message.
+
+def decrypt(text, shift):
+    text = list(text)
+    message = []
+    for i in text:
+        n = 0
+        for j in alphabet:
+            if j == i:
+                break
+            n += 1
+        message += alphabet[(n - shift) % len(alphabet)]
+    message = ''.join(message)
+    print(f"The decoded text is {message}")
+
+if direction == "encode":
+    encrypt(text, shift)
+else:
+    decrypt(text, shift)
+
