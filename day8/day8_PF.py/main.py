7f10c057ded9b3752e8354abe863a34954a05302
vivivaz <vivianleocardovaz@gmail.com>
2023-05-21 21:47:58 -0300

    day 8.PF

---
+++ day8/day8_PF.py/main.py
@@ -0,0 +1,50 @@
+#TODO-1: Import and print the logo from art.py when the program starts.
+from art import logo
+print(logo)
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+#TODO-2: What if the user enters a shift that is greater than the number of letters in the alphabet?
+#Try running the program and entering a shift number of 45.
+#Add some code so that the program continues to work even if the user enters a shift number greater than 26. 
+#Hint: Think about how you can use the modulus (%).
+
+    #TODO-3: What happens if the user enters a number/symbol/space?
+    #Can you fix the code to keep the number/symbol/space when the text is encoded/decoded?
+    #e.g. start_text = "meet me at 3"
+    #end_text = "•••• •• •• 3"
+
+def caesar(text, shift, direction):
+    text = list(text)
+    message = []
+    for i in text:
+        n = 0
+        if i in alphabet:
+            n = alphabet.index(i)
+            if direction == "encode":
+                message += alphabet[(n + shift) % len(alphabet)]
+            elif direction == "decode":
+                message += alphabet[(n - shift) % len(alphabet)]
+        else:
+            message += i
+    message = ''.join(message)
+    print(f"The {direction}d text is {message}")
+
+caesar(text, shift, direction)
+#TODO-4: Can you figure out a way to ask the user if they want to restart the cipher program?
+#e.g. Type 'yes' if you want to go again. Otherwise type 'no'.
+#If they type 'yes' then ask them for the direction/text/shift again and call the caesar() function again?
+#Hint: Try creating a while loop that continues to execute the program if the user types 'yes'. 
+resp = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
+while resp == 'yes':
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    caesar(text, shift, direction)
+    resp = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
+if resp == 'no':
+    print('Goodbye')
